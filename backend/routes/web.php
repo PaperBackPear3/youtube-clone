43f40738ebe0da30d8bc338a80b089755c2cee8e
PaperBackPear3 <fabioangeloni2001@gmail.com>
2024-07-14 01:29:40 +0200

    add aws s3

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
+
+    $contents = Storage::disk('s3')->get('file.jpg');
+    return response($contents)->header('Content-Type', 'image/jpeg');
+
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
