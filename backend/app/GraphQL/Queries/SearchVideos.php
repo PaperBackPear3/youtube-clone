d9d9b587e08a86c975ac1067461ad51d1efec563
PaperBackPear3 <fabioangeloni2001@gmail.com>
2024-07-18 22:21:22 +0200

    fix fields and query

--- backend/app/GraphQL/Queries/SearchVideos.php
+++ backend/app/GraphQL/Queries/SearchVideos.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace App\GraphQL\Queries;
 
 use App\Models\Video;
+use Illuminate\Support\Facades\DB;
 
 final readonly class SearchVideos
 {
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
-        $a = $args;
-        $data= Video::query()
-            ->whereFullText(['title', 'description'], $args['query'])
-            ->toSql();
-        return $data->get();
+        $data = Video::query()
+            ->select('*')
+            ->selectRaw('MATCH(title, description) AGAINST(? IN BOOLEAN MODE) as score', [$args['query']])
+            ->whereRaw('MATCH(title, description) AGAINST(? IN BOOLEAN MODE)', [$args['query']])
+            ->orderByDesc('score')
+            ->get();
+        return $data;
     }
 }
